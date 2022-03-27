52986200d5be42f31499711d7d9f4575b7ba38ed
ysm0706glee <ysm0706glee@gmail.com>
2022-03-27 17:49:22 +0200

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
+import { ChakraProvider, localStorageManager } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Web3ReactProvider } from "@web3-react/core";
+import {
+  ExternalProvider,
+  JsonRpcFetchFunc,
+  Web3Provider,
+} from "@ethersproject/providers";
+
+function getLibrary(provider: ExternalProvider | JsonRpcFetchFunc) {
+  return new Web3Provider(provider);
 }
 
-export default MyApp
+const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <ChakraProvider colorModeManager={localStorageManager}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Web3ReactProvider>
+  );
+};
+
+export default MyApp;
